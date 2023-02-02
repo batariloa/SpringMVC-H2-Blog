d0a133ad17060c5bfc4722df56b38f93a22721ed
andrejbatarilo <batarilocore@gmail.com>
2023-02-02 01:15:17 +0100

    Insert post fixed

--- src/main/java/com/example/springmvch2blog/controller/BlogPostController.java
+++ src/main/java/com/example/springmvch2blog/controller/BlogPostController.java
@@ -1,12 +1,15 @@
 package com.example.springmvch2blog.controller;
 
 
+import com.example.springmvch2blog.config.UsernamePasswordFilter;
 import com.example.springmvch2blog.dto.BlogPostDto;
 import com.example.springmvch2blog.dto.CreatePostRequest;
 import com.example.springmvch2blog.dto.PostsResponse;
 import com.example.springmvch2blog.dto.UserDto;
 import com.example.springmvch2blog.service.BlogPostService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,14 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/post")
 @RequiredArgsConstructor
 public class BlogPostController {
-
-
     private final BlogPostService blogPostService;
+    private static final Logger logger = LoggerFactory.getLogger(BlogPostController.class);
 
     @PostMapping
     public BlogPostDto createNewPost(@AuthenticationPrincipal UserDto user, @RequestBody CreatePostRequest createPostRequest){
 
 
+        logger.warn("Text"+ createPostRequest.getText() + " title " + createPostRequest.getTitle());
         return blogPostService.createPostForUser(createPostRequest, user);
     }
     @GetMapping
